1a086818ae57cf12c2e0d88502fb06785f41db8a
cosmoscoin <cosmoscoin.com@gmail.com>
2013-06-30 20:40:49 +0800

    create

--- p2pool.bat
+++ p2pool.bat
@@ -1,4 +1,6 @@
-screen -d -m -S CMCP2Pool /root/cosmoscoin/CMCP2Pool/run_p2pool.py --net CosmosCoin -f 0.5 --give-author 0
+screen -d -m -S CMCP2Pool python /root/cosmoscoin/CMCP2Pool/run_p2pool.py --net CosmosCoin -f 0.5 --give-author 0
 
+screen -x CMCP2Pool
+Ctrl+A Ctrl+D
 
 python run_p2pool.py --net CosmosCoin -f 0.5 --give-author 0
